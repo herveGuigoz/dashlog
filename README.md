023652cbf75a3e03a425c9da8bba19bed12251be
herveGuigoz <herve.guigoz@gmail.com>
2021-07-22 03:22:37 +0200

    ✨ feat: template

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # DASHLOG
 
-Inspired by [auto-changelog](https://github.com/cookpete/auto-changelog) and [Release](https://github.com/vercel/release)
+Command line tool for generating a changelog from git tags and semantic commit history.
 
-[Git Format](https://git-scm.com/docs/git-log#Documentation/git-log.txt-emHem)
+## Install
 
-`git log 2861f87602c0ccbe55cb595e7c485459185a8766...5c4242d55cc0880a710ead4f213ed0817d0ae532 --oneline`
+```
+$ dart pub global activate dashlog
+```
+
+## Semantic Commit Messages
+
+> Format: `<type>(<scope>): <subject>` 
+
+```
+feat: add hat wobble
+^--^  ^------------^
+|     |
+|     +-> Summary in present tense.
+|
++-------> Type: chore, docs, feat, fix, refactor, style, or test.
+
+NB: scope is optional
+```
+
+References:
+
+- https://www.conventionalcommits.org/
--- lib/src/commit.dart
+++ lib/src/commit.dart
@@ -1,16 +1,16 @@
-// git log 2861f87602c0ccbe55cb595e7c485459185a8766...5c4242d55cc0880a710ead4f213ed0817d0ae532 --oneline --pretty="%h%s"
-
 import 'package:dashlog/src/tag.dart';
 
-import '../dashlog.dart';
+import 'dashlog.dart';
 
-// TODO Date
-class Commit {
-  const Commit._(this.hash, this.type, this.name);
+class Commit extends Comparable<Commit> {
+  Commit._(this.hash, this.type, this.name);
 
   final String hash;
   final String type;
   final String name;
+
+  @override
+  int compareTo(Commit other) => type.compareTo(other.type);
 
   @override
   String toString() => 'Commit(hash: $hash, type: $type, name: $name)';
@@ -18,7 +18,7 @@ class Commit {
 
 /// Get list of tag.
 Future<List<Commit>> getCommits({required Tag start, required Tag end}) async {
-  final lines = await Dashlog.runCommand(
+  final lines = await runCommand(
     'git',
     ['log', '${start.hash}...${end.hash}', '-E', '--format=%H;%s;'],
   );
@@ -26,7 +26,7 @@ Future<List<Commit>> getCommits({required Tag start, required Tag end}) async {
   return _parseCommits(lines);
 }
 
-/// Parse [String] to [Commit]
+/// Cast list of [String] to list of [Commit]
 List<Commit> _parseCommits(List<String> lines) {
   final commits = <Commit>[];
   final regex = RegExp(r'^(.+);(.+):(.+);');
@@ -40,5 +40,5 @@ List<Commit> _parseCommits(List<String> lines) {
     }
   }
 
-  return commits;
+  return commits..sort();
 }
